77de2ad321b879e3c7699fc4246ff40684f1c87f
srijankulal <srijankulal1010@gmail.com>
2025-03-27 23:11:26 +0530

    menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,68 @@
 // import Contact from "@/components/contact"
-import FloatButtons from "@/components/FloatingButtons";
 import Header from "@/components/Header";
-import TerminalSnippet from "@/components/TerminalSnippet";
+import  TerminalSnippet  from "@/components/TerminalSnippet"; // Adjusted to match named export
 import { Button } from "@/components/ui/button";
 
-// import Hero from "@/components/hero";
-// import Projects from "@/components/Projects";
-// import Skills from "@/components/Skills";
-// import Navbar from "@/components/ui/nav-bar";
-// //import Header from "@/components/Header";
+
+
 
 
 export default function Home() {
   return (
-    <><div className="border border-gray-300 p-4 max-w-4xl mx-auto my-4">
+    <><div className="border border-gray-300 p-4   my-4">
     <Header />
     <div className="py-8 text-center border-b border-gray-300">
-      <div className="flex flex-col justify-center items-center w-full pt-8">
-        <div className="max-w-2xl w-full">
-          <h2 className="text-5xl font-bold mb-2 text-left">
-        Hi,
-          </h2>
-          <h2 className="text-5xl font-bold mb-4 text-left">
-        I&apos;m Srijan K!
-          </h2>
-                <p className="text-xl mb-4 text-left">
-                Crafting modern web experiences with security, efficiency, and scalability in mind.
-                </p>
-
-               <div className="flex  text-left">
+      <div className="flex flex-col justify-center items-center w-full ">
+            <div className="flex flex-col justify-center items-center w-full snap-start sm:snap-align-none pt-30 pb-54">
+        
+                <div className="text-left">
+                <h2 className="text-6xl font-bold mb-2 text-left">
+                <span className="inline-block">&#62;_</span>HI
+                </h2>
+                <h2 className="text-6xl font-bold mb-4 text-left pl-11">
+                I&apos;m <u>Srijan K!</u>
+                </h2>
+              <p className="text-2xl mb-4 text-left pl-11">
+              Crafting modern web experiences with security, efficiency, and scalability in mind.
+              </p>
+              <div className="flex text-left pl-11">
                 <div className="flex pr-8 sm:flex-col">
-                  <Button variant="outline" className="mt-2 py-2 text-lg font-medium h-12 px-8">Projects</Button>
+                <Button variant="outline" className="mt-2 py-2 text-lg font-medium h-12 px-8">
+                  Projects
+                </Button>
                 </div>
-                <div className="flex  pl-2 sm:flex-col">
-                  <Button variant="outline" className="mt-2 py-2 text-lg font-medium h-12 px-8">Contact</Button>
-                  </div>
+                <div className="flex pl-2 sm:flex-col">
+                <Button variant="outline" className="mt-2 py-2 text-lg font-medium h-12 px-8">
+                  Contact
+                </Button>
                 </div>
-                </div>
-                </div>
-                <div className="flex flex-col justify-center items-center w-full pt-8">
-                  <div className="max-w-2xl w-full">
-                    <h2 className="text-5xl font-bold mb-2 text-left">About Me</h2>
-                    <p className="text-xl mb-4 text-left">
+              </div>
+              </div>
+            </div>
+          </div>
+                <div className="flex flex-col items-start w-full pt-8">
+                 <div className="text-left">
+                  <h2 className="text-5xl font-bold mb-2 text-left" id="about">About Me</h2>
+                 </div>
+                    <div className="flex flex-row justify-center items-center w-full pt-8">
+                      <div className="flex flex-col justify-center items-center w-full pt-8">
+                    <p className=" text-xl mb-4 text-left">
                       I am a backend-focused software developer skilled in Python (Flask), Next js, Flutter and databases like PostgreSQL and MySQL. I specialize in building secure, scalable applications and have experience with real-time features like WebSockets. I also have some experience with app development and frontend technologies, but my strength lies in backend development. I am always eager to learn new technologies and improve my skills.
                       </p>
                     <p className="text-xl mb-4 text-left">
                     Currently, I am pursuing a Bachelor’s in Computer Applications at St. Aloysius University (2023-2026). I am always looking to learn new things and improve my skills, and I am excited to take on new challenges.
                       </p>
-                      <div className="flex  text-left">
+                      </div>
+                      <div className="  text-left">
                       <TerminalSnippet />
                       </div>
+            
                     </div>
                     </div>
                 
             </div>
     
-  <FloatButtons whereAt="Home" />
+  
   </div>
       </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,62 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+
 export default function Header() {
     return (
-        <header className="border-b border-t border-gray-300  py-2">
+        <header className="border-b border-gray-300 py-2 pb-4">
+        <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold">|  </h1>
+        <h1 className="text-4xl font-bold pl-2"> Srijan K.</h1>
+        <nav className="flex space-x-4 ml-auto pr-2">
+        <TooltipProvider>
+        <Tooltip>
+            <TooltipTrigger>
+            <a href="#about" className="text-white text-2xl"><span className="text-amber-600"><u><b>A</b></u></span><u>bout</u></a>
+            </TooltipTrigger>
+            <TooltipContent>
+      <p>Says about me!!</p>
+    </TooltipContent>    </Tooltip>
+    </TooltipProvider>
 
-            <div className="flex justify-between items-center">
-                <h1 className="text-2xl font-bold">|  Srijan K.</h1>
-                <h1 className="text-2xl font-bold">|</h1>
-            </div>
-            </header>
-            
-        //     
-        // </header>
+            <TooltipProvider>
+        <Tooltip>                        
+    <TooltipTrigger>
+            <a href="#projects" className="text-white text-2xl"><span className="text-amber-600"><u><b>P</b></u></span><u>rojects</u></a>
+           </TooltipTrigger><TooltipContent>
+        <p>Projects Made!!</p>
+    </TooltipContent>    </Tooltip>
+    </TooltipProvider>
+
+    <TooltipProvider>
+    <Tooltip>
+    <TooltipTrigger> 
+            <a href="#skills" className="text-white text-2xl"><span className="text-amber-600"><u><b>S</b></u></span><u>kills</u></a>
+            </TooltipTrigger>
+            <TooltipContent>
+        <p>About my skills</p>
+    </TooltipContent>    </Tooltip>
+    </TooltipProvider>
+
+    <TooltipProvider>
+    <Tooltip>
+    <TooltipTrigger>
+            <a href="#contact" className="text-white text-2xl"><u><span className="text-amber-600"><u><b>C</b></u></span>ontact</u></a>
+        </TooltipTrigger><TooltipContent>
+        <p>Say hello to me!!</p>
+    </TooltipContent>
+    </Tooltip>
+    </TooltipProvider>
+
+        </nav>
+        <h1 className="text-4xl font-bold"> |</h1>
+        </div>
+        
+    </header>
+
     );
+
 }
